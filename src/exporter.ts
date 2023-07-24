d1ef94114af5cdc73d1df00461f649c3bf04c1a1
Christian Galsterer <christian.galsterer@gmx.de>
2023-07-24 13:01:43 +0200

    Initial project setup

--- src/exporter.ts
+++ src/exporter.ts
@@ -1,6 +1,54 @@
 import { Gauge, Histogram } from 'prom-client'
 import type { Registry } from 'prom-client'
-import type { MongoClient } from 'mongodb'
+import type { ConnectionPoolCreatedEvent, MongoClient } from 'mongodb'
+
+// pool metrics
+const poolSize = new Gauge({
+  name: 'mongodb_driver_pool_size',
+  help: 'the current size of the connection pool, including idle and and in-use members',
+  labelNames: [
+    'server_address'
+  ]
+})
+const minSize = new Gauge({
+  name: 'mongodb_driver_pool_min',
+  help: 'the minimum size of the connection pool',
+  labelNames: [
+    'server_address'
+  ]
+})
+const maxSize = new Gauge({
+  name: 'mongodb_driver_pool_max',
+  help: 'the maximum size of the connection pool',
+  labelNames: [
+    'server_address'
+  ]
+})
+const checkedOut = new Gauge({
+  name: 'mongodb_driver_pool_checkedout',
+  help: 'the count of connections that are currently in use',
+  labelNames: [
+    'server_address'
+  ]
+})
+const waitQueueSize = new Gauge({
+  name: 'mongodb_driver_pool_waitqueuesize',
+  help: 'the current size of the wait queue for a connection from the pool',
+  labelNames: [
+    'server_address'
+  ]
+})
+
+// command metrics
+const commands = new Histogram({
+  name: 'mongodb_driver_commands_seconds',
+  help: 'Timer of mongodb commands',
+  labelNames: [
+    'command',
+    'server_address',
+    'status'
+  ]
+})
 
 /**
  * Exposes metrics for the provided Mongo client in prometheus format.
@@ -21,54 +69,6 @@ export function monitorMongoDBDriver (mongoClient: MongoClient, register: Regist
 
   const monitorCommands = mongoClient.options.monitorCommands.valueOf()
 
-  // pool metrics
-  const poolSize = new Gauge({
-    name: 'mongodb_driver_pool_size',
-    help: 'the current size of the connection pool, including idle and and in-use members',
-    labelNames: [
-      'server_address'
-    ]
-  })
-  const minSize = new Gauge({
-    name: 'mongodb_driver_pool_min',
-    help: 'the minimum size of the connection pool',
-    labelNames: [
-      'server_address'
-    ]
-  })
-  const maxSize = new Gauge({
-    name: 'mongodb_driver_pool_max',
-    help: 'the maximum size of the connection pool',
-    labelNames: [
-      'server_address'
-    ]
-  })
-  const checkedOut = new Gauge({
-    name: 'mongodb_driver_pool_checkedout',
-    help: 'the count of connections that are currently in use',
-    labelNames: [
-      'server_address'
-    ]
-  })
-  const waitQueueSize = new Gauge({
-    name: 'mongodb_driver_pool_waitqueuesize',
-    help: 'the current size of the wait queue for a connection from the pool',
-    labelNames: [
-      'server_address'
-    ]
-  })
-
-  // command metrics
-  const commands = new Histogram({
-    name: 'mongodb_driver_commands_seconds',
-    help: 'Timer of mongodb commands',
-    labelNames: [
-      'command',
-      'server_address',
-      'status'
-    ]
-  })
-
   // register metrics
   register.registerMetric(poolSize)
   register.registerMetric(minSize)
@@ -80,11 +80,7 @@ export function monitorMongoDBDriver (mongoClient: MongoClient, register: Regist
   }
 
   // pool metrics
-  mongoClient.on('connectionPoolCreated', (event) => { poolSize.set({ server_address: event.address }, 0) })
-  mongoClient.on('connectionPoolCreated', (event) => { minSize.set({ server_address: event.address }, event.options!.minPoolSize) })
-  mongoClient.on('connectionPoolCreated', (event) => { maxSize.set({ server_address: event.address }, event.options!.maxPoolSize) })
-  mongoClient.on('connectionPoolCreated', (event) => { checkedOut.set({ server_address: event.address }, 0) })
-  mongoClient.on('connectionPoolCreated', (event) => { waitQueueSize.set({ server_address: event.address }, 0) })
+  mongoClient.on('connectionPoolCreated', (event) => { onConnectionPoolCreated(event) })
   mongoClient.on('connectionCreated', (event) => { poolSize.inc({ server_address: event.address }) })
   mongoClient.on('connectionClosed', (event) => { poolSize.dec({ server_address: event.address }) })
   mongoClient.on('connectionCheckOutStarted', (event) => { waitQueueSize.inc({ server_address: event.address }) })
@@ -106,4 +102,14 @@ export function monitorMongoDBDriver (mongoClient: MongoClient, register: Regist
     mongoClient.on('commandFailed', (event) => { commands.observe({ command: event.commandName, server_address: event.address, status: 'FAILED' }, event.duration * 1000) })
     console.log('Successfully enabled command metrics for the MongoDB Node.js driver.')
   }
+}
+
+function onConnectionPoolCreated (event: ConnectionPoolCreatedEvent): void {
+  poolSize.set({ server_address: event.address }, 0)
+  minSize.set({ server_address: event.address }, event.options!.minPoolSize)
+  maxSize.set({ server_address: event.address }, event.options!.maxPoolSize)
+  checkedOut.set({ server_address: event.address }, 0)
+  waitQueueSize.set({ server_address: event.address }, 0)
+
+  console.log('connection pool created')
 }
