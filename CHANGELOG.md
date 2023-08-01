248984156a9c86908b460c0f5e825099f37078eb
Christian Galsterer <christian.galsterer@gmx.de>
2023-08-01 11:04:00 +0200

    Initial project setup

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.2.0] 2023-08-01
+ 
+### Added
+
+- Added possibility to specify the buckets for the mongodb_driver_commands_seconds_bucket metric via the new optional parameter MongoDBDriverExporterOptions
+
+### Fixed
+
+- Fixed incorrect calculations of the histogram for the mongodb_driver_commands_seconds_bucket metric. Default buckets are now [0.001, 0.005, 0.010, 0.020, 0.030, 0.040, 0.050, 0.100, 0.200, 0.500, 1.0, 2.0, 5.0, 10]
--- src/exporter.ts
+++ src/exporter.ts
@@ -1,151 +1,187 @@
 import { Gauge, Histogram, type Registry } from 'prom-client'
 import type { ConnectionCreatedEvent, ConnectionClosedEvent, ConnectionPoolCreatedEvent, ConnectionCheckOutStartedEvent, ConnectionCheckedOutEvent, ConnectionCheckOutFailedEvent, ConnectionCheckedInEvent, ConnectionPoolClosedEvent, CommandSucceededEvent, CommandFailedEvent, MongoClient } from 'mongodb'
 
-// pool metrics
-const poolSize = new Gauge({
-  name: 'mongodb_driver_pool_size',
-  help: 'the current size of the connection pool, including idle and and in-use members',
-  labelNames: [
-    'server_address'
-  ]
-})
-const minSize = new Gauge({
-  name: 'mongodb_driver_pool_min',
-  help: 'the minimum size of the connection pool',
-  labelNames: [
-    'server_address'
-  ]
-})
-const maxSize = new Gauge({
-  name: 'mongodb_driver_pool_max',
-  help: 'the maximum size of the connection pool',
-  labelNames: [
-    'server_address'
-  ]
-})
-const checkedOut = new Gauge({
-  name: 'mongodb_driver_pool_checkedout',
-  help: 'the count of connections that are currently in use',
-  labelNames: [
-    'server_address'
-  ]
-})
-const waitQueueSize = new Gauge({
-  name: 'mongodb_driver_pool_waitqueuesize',
-  help: 'the current size of the wait queue for a connection from the pool',
-  labelNames: [
-    'server_address'
-  ]
-})
-
 /**
  * Exposes metrics for the provided Mongo client in prometheus format.
  *
  * @param mongoClient The mongoClient for which to expose metrics in prometheus format.
  * @param register the prometheus registry used to expose the metrics.
+ * @param options optional parameters to configure the exporter
  */
 export function monitorMongoDBDriver (mongoClient: MongoClient, register: Registry, options?: MongoDBDriverExporterOptions): void {
-  if (mongoClient == null) {
-    options?.logger?.error('mongoClient is null or undefined. No metrics can be exported.')
-    return
-  }
+  const exporter = new MongoDBDriverExporter(mongoClient, register, options)
+  exporter.registerMetrics()
+  exporter.enableMetrics()
+}
 
-  if (register == null) {
-    options?.logger?.error('register is null or undefined. No metrics can be exported.')
-    return
-  }
-
-  // command metrics
-  const commandBuckets = options?.commandHistogramBuckets != null ? options.commandHistogramBuckets : [0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10, 50, 100, 200, 500, 1000, 2000, 5000]
-  const commands = new Histogram({
-    name: 'mongodb_driver_commands_seconds',
-    help: 'Timer of mongodb commands',
-    buckets: commandBuckets,
-    labelNames: [
-      'command',
-      'server_address',
-      'status'
-    ]
-  })
-
-  const monitorCommands = mongoClient.options.monitorCommands.valueOf()
-
-  // register metrics
-  register.registerMetric(poolSize)
-  register.registerMetric(minSize)
-  register.registerMetric(maxSize)
-  register.registerMetric(checkedOut)
-  register.registerMetric(waitQueueSize)
-  if (monitorCommands) {
-    register.registerMetric(commands)
-  }
+class MongoDBDriverExporter {
+  private readonly register: Registry
+  private readonly mongoClient: MongoClient
+  private readonly options: MongoDBDriverExporterOptions | undefined
 
   // pool metrics
-  mongoClient.on('connectionPoolCreated', (event) => { onConnectionPoolCreated(event) })
-  mongoClient.on('connectionPoolClosed', (event) => { onConnectionPoolClosed(event) })
-  mongoClient.on('connectionCreated', (event) => { onConnectionCreated(event) })
-  mongoClient.on('connectionClosed', (event) => { onConnectionClosed(event) })
-  mongoClient.on('connectionCheckOutStarted', (event) => { onConnectionCheckOutStarted(event) })
-  mongoClient.on('connectionCheckedOut', (event) => { onConnectionCheckedOut(event) })
-  mongoClient.on('connectionCheckOutFailed', (event) => { onConnectionCheckOutFailed(event) })
-  mongoClient.on('connectionCheckedIn', (event) => { onConnectionCheckedIn(event) })
-  options?.logger?.info('Successfully enabled connection pool metrics for the MongoDB Node.js driver.')
+  private readonly poolSize: Gauge
+  private readonly minSize: Gauge
+  private readonly maxSize: Gauge
+  private readonly checkedOut: Gauge
+  private readonly waitQueueSize: Gauge
 
   // command metrics
-  if (monitorCommands) {
-    mongoClient.on('commandSucceeded', (event) => { onCommandSucceeded(event, commands) })
-    mongoClient.on('commandFailed', (event) => { onCommandFailed(event, commands) })
-    options?.logger?.info('Successfully enabled command metrics for the MongoDB Node.js driver.')
+  private readonly commands: Histogram
+
+  constructor (mongoClient: MongoClient, register: Registry, options?: MongoDBDriverExporterOptions) {
+    if (mongoClient == null) {
+      options?.logger?.error('mongoClient is null or undefined. No metrics can be exported.')
+      return
+    }
+    this.mongoClient = mongoClient
+
+    if (register == null) {
+      options?.logger?.error('register is null or undefined. No metrics can be exported.')
+      return
+    }
+    this.register = register
+    this.options = options
+
+    this.poolSize = new Gauge({
+      name: 'mongodb_driver_pool_size',
+      help: 'the current size of the connection pool, including idle and and in-use members',
+      labelNames: [
+        'server_address'
+      ]
+    })
+
+    this.minSize = new Gauge({
+      name: 'mongodb_driver_pool_min',
+      help: 'the minimum size of the connection pool',
+      labelNames: [
+        'server_address'
+      ]
+    })
+
+    this.maxSize = new Gauge({
+      name: 'mongodb_driver_pool_max',
+      help: 'the maximum size of the connection pool',
+      labelNames: [
+        'server_address'
+      ]
+    })
+
+    this.checkedOut = new Gauge({
+      name: 'mongodb_driver_pool_checkedout',
+      help: 'the count of connections that are currently in use',
+      labelNames: [
+        'server_address'
+      ]
+    })
+
+    this.waitQueueSize = new Gauge({
+      name: 'mongodb_driver_pool_waitqueuesize',
+      help: 'the current size of the wait queue for a connection from the pool',
+      labelNames: [
+        'server_address'
+      ]
+    })
+
+    this.commands = new Histogram({
+      name: 'mongodb_driver_commands_seconds',
+      help: 'Timer of mongodb commands',
+      buckets: this.commandHistogramBuckets(),
+      labelNames: [
+        'command',
+        'server_address',
+        'status'
+      ]
+    })
   }
-}
 
-function onConnectionPoolCreated (event: ConnectionPoolCreatedEvent): void {
-  poolSize.set({ server_address: event.address }, 0)
-  minSize.set({ server_address: event.address }, event.options!.minPoolSize)
-  maxSize.set({ server_address: event.address }, event.options!.maxPoolSize)
-  checkedOut.set({ server_address: event.address }, 0)
-  waitQueueSize.set({ server_address: event.address }, 0)
-}
+  enableMetrics (): void {
+    this.mongoClient.on('connectionPoolCreated', (event) => { this.onConnectionPoolCreated(event) })
+    this.mongoClient.on('connectionPoolClosed', (event) => { this.onConnectionPoolClosed(event) })
+    this.mongoClient.on('connectionCreated', (event) => { this.onConnectionCreated(event) })
+    this.mongoClient.on('connectionClosed', (event) => { this.onConnectionClosed(event) })
+    this.mongoClient.on('connectionCheckOutStarted', (event) => { this.onConnectionCheckOutStarted(event) })
+    this.mongoClient.on('connectionCheckedOut', (event) => { this.onConnectionCheckedOut(event) })
+    this.mongoClient.on('connectionCheckOutFailed', (event) => { this.onConnectionCheckOutFailed(event) })
+    this.mongoClient.on('connectionCheckedIn', (event) => { this.onConnectionCheckedIn(event) })
+    this.options?.logger?.info('Successfully enabled connection pool metrics for the MongoDB Node.js driver.')
 
-function onConnectionCreated (event: ConnectionCreatedEvent): void {
-  poolSize.inc({ server_address: event.address })
-}
+    // command metrics
+    if (this.monitorCommands()) {
+      this.mongoClient.on('commandSucceeded', (event) => { this.onCommandSucceeded(event) })
+      this.mongoClient.on('commandFailed', (event) => { this.onCommandFailed(event) })
+      this.options?.logger?.info('Successfully enabled command metrics for the MongoDB Node.js driver.')
+    }
+  }
 
-function onConnectionClosed (event: ConnectionClosedEvent): void {
-  poolSize.dec({ server_address: event.address })
-}
+  registerMetrics (): void {
+    this.register.registerMetric(this.poolSize)
+    this.register.registerMetric(this.minSize)
+    this.register.registerMetric(this.maxSize)
+    this.register.registerMetric(this.checkedOut)
+    this.register.registerMetric(this.waitQueueSize)
+    if (this.monitorCommands()) {
+      this.register.registerMetric(this.commands)
+    }
+  }
 
-function onConnectionCheckOutStarted (event: ConnectionCheckOutStartedEvent): void {
-  waitQueueSize.inc({ server_address: event.address })
-}
+  private monitorCommands (): boolean {
+    return this.mongoClient.options.monitorCommands.valueOf()
+  }
 
-function onConnectionCheckedOut (event: ConnectionCheckedOutEvent): void {
-  checkedOut.inc({ server_address: event.address })
-  waitQueueSize.dec({ server_address: event.address })
-}
+  private commandHistogramBuckets (): number[] {
+    if (this.options?.mongodbDriverCommandsSecondsHistogramBuckets != null) return this.options.mongodbDriverCommandsSecondsHistogramBuckets
+    else return [0.001, 0.005, 0.010, 0.020, 0.030, 0.040, 0.050, 0.100, 0.200, 0.500, 1.0, 2.0, 5.0, 10]
+  }
 
-function onConnectionCheckOutFailed (event: ConnectionCheckOutFailedEvent): void {
-  waitQueueSize.dec({ server_address: event.address })
-}
+  private onConnectionPoolCreated (event: ConnectionPoolCreatedEvent): void {
+    this.poolSize.set({ server_address: event.address }, 0)
+    this.minSize.set({ server_address: event.address }, event.options!.minPoolSize)
+    this.maxSize.set({ server_address: event.address }, event.options!.maxPoolSize)
+    this.checkedOut.set({ server_address: event.address }, 0)
+    this.waitQueueSize.set({ server_address: event.address }, 0)
+  }
 
-function onConnectionCheckedIn (event: ConnectionCheckedInEvent): void {
-  checkedOut.dec({ server_address: event.address })
-}
+  private onConnectionCreated (event: ConnectionCreatedEvent): void {
+    this.poolSize.inc({ server_address: event.address })
+  }
 
-function onConnectionPoolClosed (event: ConnectionPoolClosedEvent): void {
-  poolSize.set({ server_address: event.address }, 0)
-  minSize.reset()
-  maxSize.reset()
-  checkedOut.reset()
-  waitQueueSize.reset()
-}
+  private onConnectionClosed (event: ConnectionClosedEvent): void {
+    this.poolSize.dec({ server_address: event.address })
+  }
 
-function onCommandSucceeded (event: CommandSucceededEvent, commands: Histogram): void {
-  commands.observe({ command: event.commandName, server_address: event.address, status: 'SUCCESS' }, event.duration)
-}
+  private onConnectionCheckOutStarted (event: ConnectionCheckOutStartedEvent): void {
+    this.waitQueueSize.inc({ server_address: event.address })
+  }
 
-function onCommandFailed (event: CommandFailedEvent, commands: Histogram): void {
-  commands.observe({ command: event.commandName, server_address: event.address, status: 'FAILED' }, event.duration)
+  private onConnectionCheckedOut (event: ConnectionCheckedOutEvent): void {
+    this.checkedOut.inc({ server_address: event.address })
+    this.waitQueueSize.dec({ server_address: event.address })
+  }
+
+  private onConnectionCheckOutFailed (event: ConnectionCheckOutFailedEvent): void {
+    this.waitQueueSize.dec({ server_address: event.address })
+  }
+
+  private onConnectionCheckedIn (event: ConnectionCheckedInEvent): void {
+    this.checkedOut.dec({ server_address: event.address })
+  }
+
+  private onConnectionPoolClosed (event: ConnectionPoolClosedEvent): void {
+    this.poolSize.set({ server_address: event.address }, 0)
+    this.minSize.reset()
+    this.maxSize.reset()
+    this.checkedOut.reset()
+    this.waitQueueSize.reset()
+  }
+
+  private onCommandSucceeded (event: CommandSucceededEvent): void {
+    this.commands.observe({ command: event.commandName, server_address: event.address, status: 'SUCCESS' }, event.duration / 1000)
+  }
+
+  private onCommandFailed (event: CommandFailedEvent): void {
+    this.commands.observe({ command: event.commandName, server_address: event.address, status: 'FAILED' }, event.duration / 1000)
+  }
 }
 
 /**
@@ -153,7 +189,10 @@ function onCommandFailed (event: CommandFailedEvent, commands: Histogram): void 
  */
 export interface MongoDBDriverExporterOptions {
   logger?: Logger
-  commandHistogramBuckets?: number[]
+  /**
+   * Buckets for the mongodb_driver_commands_seconds_bucket metric. Default buckets are [0.001, 0.005, 0.010, 0.020, 0.030, 0.040, 0.050, 0.100, 0.200, 0.500, 1.0, 2.0, 5.0, 10]
+   */
+  mongodbDriverCommandsSecondsHistogramBuckets?: number[]
 }
 
 /**
