eb2662342181c74dcc2f5366f042d37aec6691af
Christian Galsterer <christian.galsterer@gmx.de>
2023-08-06 13:02:42 +0200

    test: test coverage exporter tests

--- test/exporter.spec.ts
+++ test/exporter.spec.ts
@@ -22,12 +22,17 @@ describe('tests monitorMongoDBDriver', () => {
     mockMongoDBDriverExporter.mockClear()
   })
 
-  test('tests if monitorMongoDBDriver called MongoDBDriverExporter constructor', () => {
+  test('tests if monitorMongoDBDriver called MongoDBDriverExporter constructor with mandatory parameter', () => {
     monitorMongoDBDriver(mongoClient, register)
     expect(mockMongoDBDriverExporter).toHaveBeenCalledTimes(1)
-    expect(mockMongoDBDriverExporter.mock.calls.length).toBe(1)
-    expect(mockMongoDBDriverExporter.mock.calls[0][0]).toBe(mongoClient)
-    expect(mockMongoDBDriverExporter.mock.calls[0][1]).toBe(register)
+    expect(mockMongoDBDriverExporter).toBeCalledWith(mongoClient, register, undefined)
+  })
+
+  test('tests if monitorMongoDBDriver called MongoDBDriverExporter constructor with optional parameter', () => {
+    const options = { mongodbDriverCommandsSecondsHistogramBuckets: [0.001, 0.005, 0.010, 0.020, 0.030, 0.040, 0.050, 0.100, 0.200, 0.500, 1.0, 2.0, 5.0, 20] }
+    monitorMongoDBDriver(mongoClient, register, options)
+    expect(mockMongoDBDriverExporter).toHaveBeenCalledTimes(1)
+    expect(mockMongoDBDriverExporter).toBeCalledWith(mongoClient, register, options)
   })
 
   test('tests if monitorMongoDBDriver called methods of MongoDBDriverExporter instance', () => {
