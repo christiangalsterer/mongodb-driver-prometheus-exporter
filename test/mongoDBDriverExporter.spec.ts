ce4aaa2e11c7c5a4bb4f42ffd12e2a7942715234
Christian Galsterer <christian.galsterer@gmx.de>
2023-08-09 18:00:00 +0200

    test: increase test coverage MongoDBDriverExporter

--- test/mongoDBDriverExporter.spec.ts
+++ test/mongoDBDriverExporter.spec.ts
@@ -23,6 +23,20 @@ describe('tests mongoDBDriverExporter', () => {
     expect(register.getSingleMetric('mongodb_driver_commands_seconds')).toBeDefined()
   })
 
+  test('tests if connection and commands metrics are registered in registry with optional configurations', () => {
+    const mongoClient = new MongoClient('mongodb://localhost:27017', { monitorCommands: true })
+    const options = { mongodbDriverCommandsSecondsHistogramBuckets: [0.001, 0.005, 0.010, 0.020, 0.030, 0.040, 0.050, 0.100, 0.200, 0.500, 1.0, 2.0, 5.0, 20], defaultLabels: { foo: 'bar', alice: 2 } }
+    const exporter = new MongoDBDriverExporter(mongoClient, register, options)
+    exporter.enableMetrics()
+    expect(register.getMetricsAsArray().length).toBe(6)
+    expect(register.getSingleMetric('mongodb_driver_pool_size')).toBeDefined()
+    expect(register.getSingleMetric('mongodb_driver_pool_min')).toBeDefined()
+    expect(register.getSingleMetric('mongodb_driver_pool_max')).toBeDefined()
+    expect(register.getSingleMetric('mongodb_driver_pool_checkedout')).toBeDefined()
+    expect(register.getSingleMetric('mongodb_driver_pool_waitqueuesize')).toBeDefined()
+    expect(register.getSingleMetric('mongodb_driver_commands_seconds')).toBeDefined()
+  })
+
   test('tests if only connection metrics are registered in registry', () => {
     const mongoClient = new MongoClient('mongodb://localhost:27017', { monitorCommands: false })
     const exporter = new MongoDBDriverExporter(mongoClient, register)
@@ -39,8 +53,7 @@ describe('tests mongoDBDriverExporter', () => {
 
   test('tests if event connection and command listeners are registered for mongo client events', () => {
     const mongoClient = new MongoClient('mongodb://localhost:27017', { monitorCommands: true })
-    const options = { mongodbDriverCommandsSecondsHistogramBuckets: [0.001, 0.005, 0.010, 0.020, 0.030, 0.040, 0.050, 0.100, 0.200, 0.500, 1.0, 2.0, 5.0, 20], defaultLabels: { foo: 'bar', alice: 2 } }
-    const exporter = new MongoDBDriverExporter(mongoClient, register, options)
+    const exporter = new MongoDBDriverExporter(mongoClient, register)
     exporter.enableMetrics()
     expect(mongoClient.listenerCount('connectionPoolCreated')).toBe(1)
     expect(mongoClient.listenerCount('connectionPoolClosed')).toBe(1)
