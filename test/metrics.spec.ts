51dd45a7896f1216f63e170aee86fb3acd8660a3
Christian Galsterer <christian.galsterer@gmx.de>
2023-10-26 16:11:22 +0200

    test: add tests to check that metrics are created with correct parameter

--- test/metrics.spec.ts
+++ test/metrics.spec.ts
@@ -21,6 +21,7 @@ jest.mock('prom-client', () => ({
 }))
 
 describe('test if all metrics are created with the correct parameters', () => {
+  const options = { defaultLabels: { foo: 'bar', alice: 2 } }
   const mongoClient = new MongoClient('mongodb://localhost:27017', { monitorCommands: true })
   const register: Registry = {} as Registry
 
@@ -28,11 +29,12 @@ describe('test if all metrics are created with the correct parameters', () => {
     jest.resetAllMocks()
   })
 
-  test('tests if all gauge metrics are created', () => {
+  test('tests if all metrics are created', () => {
     // eslint-disable-next-line no-new
     new MongoDBDriverExporter(mongoClient, register)
 
     expect(Gauge).toBeCalledTimes(5)
+    expect(Histogram).toBeCalledTimes(1)
 
     expect(Gauge).toHaveBeenCalledWith({
       name: 'mongodb_driver_pool_size',
@@ -40,43 +42,91 @@ describe('test if all metrics are created with the correct parameters', () => {
       labelNames: ['server_address'],
       registers: [register]
     })
+
     expect(Gauge).toHaveBeenCalledWith({
       name: 'mongodb_driver_pool_min',
       help: 'the minimum size of the connection pool',
       labelNames: ['server_address'],
       registers: [register]
     })
+
     expect(Gauge).toHaveBeenCalledWith({
       name: 'mongodb_driver_pool_max',
       help: 'the maximum size of the connection pool',
       labelNames: ['server_address'],
       registers: [register]
     })
+
     expect(Gauge).toHaveBeenCalledWith({
       name: 'mongodb_driver_pool_checkedout',
       help: 'the count of connections that are currently in use',
       labelNames: ['server_address'],
       registers: [register]
     })
+
     expect(Gauge).toHaveBeenCalledWith({
       name: 'mongodb_driver_pool_waitqueuesize',
       help: 'the current size of the wait queue for a connection from the pool',
       labelNames: ['server_address'],
       registers: [register]
     })
-  })
-
-  test('tests if all histogram metrics are created', () => {
-    // eslint-disable-next-line no-new
-    new MongoDBDriverExporter(mongoClient, register)
-
-    expect(Histogram).toBeCalledTimes(1)
 
     expect(Histogram).toHaveBeenCalledWith({
       name: 'mongodb_driver_commands_seconds',
       help: 'Timer of mongodb commands',
       buckets: [0.001, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10],
       labelNames: ['command', 'server_address', 'status'],
+      registers: [register]
+    })
+  })
+
+  test('tests if all metrics are created with default labels', () => {
+    // eslint-disable-next-line no-new
+    new MongoDBDriverExporter(mongoClient, register, options)
+
+    expect(Gauge).toBeCalledTimes(5)
+    expect(Histogram).toBeCalledTimes(1)
+
+    expect(Gauge).toHaveBeenCalledWith({
+      name: 'mongodb_driver_pool_size',
+      help: 'the current size of the connection pool, including idle and in-use members',
+      labelNames: ['server_address', 'foo', 'alice'],
+      registers: [register]
+    })
+
+    expect(Gauge).toHaveBeenCalledWith({
+      name: 'mongodb_driver_pool_min',
+      help: 'the minimum size of the connection pool',
+      labelNames: ['server_address', 'foo', 'alice'],
+      registers: [register]
+    })
+
+    expect(Gauge).toHaveBeenCalledWith({
+      name: 'mongodb_driver_pool_max',
+      help: 'the maximum size of the connection pool',
+      labelNames: ['server_address', 'foo', 'alice'],
+      registers: [register]
+    })
+
+    expect(Gauge).toHaveBeenCalledWith({
+      name: 'mongodb_driver_pool_checkedout',
+      help: 'the count of connections that are currently in use',
+      labelNames: ['server_address', 'foo', 'alice'],
+      registers: [register]
+    })
+
+    expect(Gauge).toHaveBeenCalledWith({
+      name: 'mongodb_driver_pool_waitqueuesize',
+      help: 'the current size of the wait queue for a connection from the pool',
+      labelNames: ['server_address', 'foo', 'alice'],
+      registers: [register]
+    })
+
+    expect(Histogram).toHaveBeenCalledWith({
+      name: 'mongodb_driver_commands_seconds',
+      help: 'Timer of mongodb commands',
+      buckets: [0.001, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10],
+      labelNames: ['command', 'server_address', 'status', 'foo', 'alice'],
       registers: [register]
     })
   })
