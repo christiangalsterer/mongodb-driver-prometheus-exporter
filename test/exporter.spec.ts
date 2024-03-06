5fda9db661b140d8019074d0d8213ea6b3df135e
Christian Galsterer <christian.galsterer@gmx.de>
2024-03-06 04:04:00 +0100

    test: enable eslint-plugin-jest

--- test/exporter.spec.ts
+++ test/exporter.spec.ts
@@ -21,14 +21,14 @@ describe('tests monitorMongoDBDriver', () => {
   test('tests if monitorMongoDBDriver called MongoDBDriverExporter constructor with mandatory parameter', () => {
     monitorMongoDBDriver(mongoClient, register)
     expect(mockMongoDBDriverExporter).toHaveBeenCalledTimes(1)
-    expect(mockMongoDBDriverExporter).toBeCalledWith(mongoClient, register, undefined)
+    expect(mockMongoDBDriverExporter).toHaveBeenCalledWith(mongoClient, register, undefined)
   })
 
   test('tests if monitorMongoDBDriver called MongoDBDriverExporter constructor with optional parameter', () => {
     const options = { mongodbDriverCommandsSecondsHistogramBuckets: [0.001, 0.005, 0.010, 0.020, 0.030, 0.040, 0.050, 0.100, 0.200, 0.500, 1.0, 2.0, 5.0, 20] }
     monitorMongoDBDriver(mongoClient, register, options)
     expect(mockMongoDBDriverExporter).toHaveBeenCalledTimes(1)
-    expect(mockMongoDBDriverExporter).toBeCalledWith(mongoClient, register, options)
+    expect(mockMongoDBDriverExporter).toHaveBeenCalledWith(mongoClient, register, options)
   })
 
   test('tests if monitorMongoDBDriver called methods of MongoDBDriverExporter instance', () => {
--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from '@jest/globals'
 
 import { ConsoleLogger, mergeLabelNamesWithStandardLabels, mergeLabelsWithStandardLabels } from '../src/utils'
 
-describe('ConsoleLogger', () => {
+describe('consoleLogger', () => {
   let consoleLogger: ConsoleLogger
 
   beforeEach(() => {
@@ -35,19 +35,19 @@ describe('tests mergeLabelNamesWithStandardLabels', () => {
   const labels = ['label1', 'label2']
   const emptylabels = []
 
-  test('test mergeLabelNamesWithStandardLabels with no default labels', () => {
+  test('mergeLabelNamesWithStandardLabels with no default labels', () => {
     expect(mergeLabelNamesWithStandardLabels(labels)).toStrictEqual(labels)
   })
 
-  test('test mergeLabelNamesWithStandardLabels with empty labels and no default labels', () => {
+  test('mergeLabelNamesWithStandardLabels with empty labels and no default labels', () => {
     expect(mergeLabelNamesWithStandardLabels(emptylabels)).toStrictEqual([])
   })
 
-  test('test mergeLabelNamesWithStandardLabels with default labels', () => {
+  test('mergeLabelNamesWithStandardLabels with default labels', () => {
     expect(mergeLabelNamesWithStandardLabels(labels, defaultLabels)).toStrictEqual(['label1', 'label2', 'foo', 'alice'])
   })
 
-  test('test mergeLabelNamesWithStandardLabels with empty labels and default labels', () => {
+  test('mergeLabelNamesWithStandardLabels with empty labels and default labels', () => {
     expect(mergeLabelNamesWithStandardLabels(emptylabels, defaultLabels)).toStrictEqual(['foo', 'alice'])
   })
 })
@@ -57,19 +57,19 @@ describe('tests mergeLabelsWithStandardLabels', () => {
   const labels = { label1: 'value1', label2: 2, label3: undefined }
   const emptyLabels = {}
 
-  test('test mergeLabelsWithStandardLabels with labels and no default labels', () => {
+  test('mergeLabelsWithStandardLabels with labels and no default labels', () => {
     expect(mergeLabelsWithStandardLabels(labels)).toStrictEqual({ label1: 'value1', label2: 2 })
   })
 
-  test('test mergeLabelsWithStandardLabels with empty labels and no default labels', () => {
+  test('mergeLabelsWithStandardLabels with empty labels and no default labels', () => {
     expect(mergeLabelsWithStandardLabels(emptyLabels)).toStrictEqual(emptyLabels)
   })
 
-  test('test mergeLabelsWithStandardLabels with labels and default labels', () => {
+  test('mergeLabelsWithStandardLabels with labels and default labels', () => {
     expect(mergeLabelsWithStandardLabels(labels, defaultLabels)).toStrictEqual({ label1: 'value1', label2: 2, foo: 'bar', alice: 3 })
   })
 
-  test('test mergeLabelsWithStandardLabels with empty labels and default labels', () => {
+  test('mergeLabelsWithStandardLabels with empty labels and default labels', () => {
     expect(mergeLabelsWithStandardLabels(emptyLabels, defaultLabels)).toStrictEqual(defaultLabels)
   })
 })
